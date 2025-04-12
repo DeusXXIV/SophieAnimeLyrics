0ad245084f4caf4a705c2a76d851e8f77c32ad3e
DeusXXIV <dcfermodev24@gmail.com>
2025-04-13 00:22:05 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Custom JavaScript for AnimeLyrics project
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('AnimeLyrics website loaded successfully!');
+    
+    // Add your JavaScript code here.
+  });
+  
